cdd67ae18aa79fb2f5399a9ed1f27e027d5c411e
Lacasx1 <silvawoo12@gmail.com>
2026-07-27 02:58:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Mpro Installer - Version v3
 
-REPO_URL="https://github.com/Lacasx1/mbpro.git"
+REPO_URL="https://github.com/Lacasx1/Mxpro.git"
 REPO_BRANCH="main"
 CMD_NAME="mpro"
 TOTAL_STEPS=7
